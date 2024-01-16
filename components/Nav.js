4b1e696de0c029df92cb93a3807b3f062683533b
David Hwang <hwangdav000@gmail.com>
2024-01-16 00:12:38 -0600

    changed navbar styling to fit aws amplify

--- components/Nav.js
+++ components/Nav.js
@@ -1,12 +1,5 @@
 // icons
-import {
-  HiHome,
-  HiUser,
-  HiViewColumns,
-  HiRectangleGroup,
-  HiChatBubbleBottomCenterText,
-  HiEnvelope,
-} from 'react-icons/hi2';
+import { HiHome, HiUser, HiViewColumns, HiEnvelope } from 'react-icons/hi2';
 
 // nav data
 export const navData = [
@@ -28,9 +21,9 @@ const Nav = () => {
   const router = useRouter();
   const pathname = router.pathname;
   return (
-    <nav className="flex flex-col items-center xl:justify-center gap-y-4 fixed h-max bottom-0 mt-auto xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen">
+    <nav className="flex flex-col items-center xl:justify-center lg:justify-center gap-y-4 fixed h-max bottom-0 mt-auto xl:right-[2%] lg:right-[2%] z-50 top-0 w-full xl:w-16 lg:w-16 xl:max-w-md lg:max-w-md xl:h-screen lg:h-screen">
       {}
-      <div className="flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-[80px] bg-gray-300 xl:h-max py-8 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full">
+      <div className="flex w-full xl:flex-col lg:flex-col items-center justify-between xl:justify-center lg:justify-center gap-y-10 px-4 md:px-40 xl:px-0 lg:px-0 h-[80px] bg-gray-300 xl:h-max lg:h-max py-8 backdrop-blur-sm text-3xl xl:text-xl lg:text-xl xl:rounded-full lg:rounded-full">
         {navData.map((link, index) => {
           return (
             <Link
